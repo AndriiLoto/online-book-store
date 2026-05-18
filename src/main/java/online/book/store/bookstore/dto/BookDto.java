95d03971d87491f164277de65a4a15a4f1a7deb0
Andrii Lotoshynskyi <wetwettew@gmail.com>
2026-05-18 06:55:31 +0300

    Added response status for PostMapping + minor changes in BookController and BookDto

--- src/main/java/online/book/store/bookstore/dto/BookDto.java
+++ src/main/java/online/book/store/bookstore/dto/BookDto.java
@@ -9,7 +9,7 @@ import lombok.ToString;
 @Setter
 @ToString
 public class BookDto {
-    private long id;
+    private Long id;
     private String title;
     private String author;
     private String isbn;
